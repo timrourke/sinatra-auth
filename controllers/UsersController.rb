b8c1ff663c832a20fdfa3cb843fa9405d365a3f4
Tim Rourke <tim@timrourke.com>
2015-07-31 12:43:22 -0500

    Completing baseline CRUD operations for users

--- controllers/UsersController.rb
+++ controllers/UsersController.rb
@@ -13,6 +13,9 @@ class UsersController < ApplicationController
 	get '/' do
 		authorization_check
 
+		@nonce = Rack::Auth::Digest::Nonce.new(Time.now, NONCE_SECRET)
+		@users = User.order("lower(user_name) DESC").all
+
 		erb :users, :locals => {'body_class' => 'users'}
 	end
 
@@ -53,7 +56,6 @@ class UsersController < ApplicationController
 			if user.password_hash == BCrypt::Engine.hash_secret(params[:user_password], user.password_salt)
 				#success, render success template
 				session[:user] = user
-				puts 'SUCCESSSSSSSSSSS!!!!!'
 				@user = session[:user]
 				
 				erb :'users/thanks-login', :locals => {'body_class' => 'users users--login'}
@@ -72,7 +74,7 @@ class UsersController < ApplicationController
 		
 		if (!@nonce.valid? || @nonce.stale?)
 			#return early if form nonce invalid
-			flash.message = "Sorry, there was a problem logging you in. Please try again."
+			flash.message = "Sorry, there was a problem adding you as a user. Please try again."
 			erb :'users/signup', :locals => {'body_class' => 'users users--signup'}
 		
 		elsif (params[:user_password].to_s != params[:user_password_confirm].to_s)
@@ -85,6 +87,7 @@ class UsersController < ApplicationController
 			@new_user.user_name = params[:user_name]
 			@new_user.user_email = params[:user_email].downcase
 			@new_user.is_admin = 0 #0 is false, 1 is true
+			@new_user.created = Time.now
 
 			password_salt = BCrypt::Engine.generate_salt
 			password_hash = BCrypt::Engine.hash_secret(params[:user_password], password_salt)
@@ -101,5 +104,116 @@ class UsersController < ApplicationController
 		end
 	end
 
+	get '/edit/:id' do
+		authorization_check
+
+		#NONCE_SECRET should be complex string, saved in /.config/nonce_configuration.rb
+		@nonce = Rack::Auth::Digest::Nonce.new(Time.now, NONCE_SECRET)
+
+		@user = User.find(params[:id])
+
+		if @user
+			erb :'users/edit', :locals => {'body_class' => 'users users--edit'}
+		else
+			redirect not_found
+		end
+	end
+
+	post '/edit' do
+		authorization_check
+
+		@nonce = Rack::Auth::Digest::Nonce.parse(params[:nonce])
+		
+		if (!@nonce.valid? || @nonce.stale?)
+			#return early if form nonce invalid
+			@user = @edited_user
+			flash.message = "Sorry, there was a problem editing the user. Please try again."
+			erb :'users/edit', :locals => {:id => params[:user_id], 'body_class' => 'users users--edit'}
+		
+		else
+			@edited_user = User.find(params[:user_id])
+			@edited_user.user_name = params[:user_name]
+			@edited_user.user_email = params[:user_email].downcase
+			@edited_user.is_admin = (params[:user_is_admin]) ? 1 : 0 #0 is false, 1 is true
+			@edited_user.modified = Time.now
+
+			if @edited_user.save
+				@user = @edited_user
+				flash.message = "You have successfully edited the user."
+				erb :'users/edit', :locals => {:id => params[:user_id], 'body_class' => 'users users--edit'}
+			else
+				@user = @edited_user
+				flash.message = "Sorry, there was a problem editing the user. Please try again."
+				erb :'users/edit', :locals => {:id => params[:user_id], 'body_class' => 'users users--edit'}
+			end
+		end
+
+	end
+
+	get '/new' do
+		authorization_check
+
+		#NONCE_SECRET should be complex string, saved in /.config/nonce_configuration.rb
+		@nonce = Rack::Auth::Digest::Nonce.new(Time.now, NONCE_SECRET)
+
+		erb :'users/new', :locals => {'body_class' => 'users users--new'}	
+	end
+
+	post '/new' do
+		authorization_check
+
+		@nonce = Rack::Auth::Digest::Nonce.parse(params[:nonce])
+		
+		if (!@nonce.valid? || @nonce.stale?)
+			#return early if form nonce invalid
+			flash.message = "Sorry, there was a problem creating the user. Please try again."
+			erb :'users/new', :locals => {'body_class' => 'users users--new'}
+		
+		elsif (params[:user_password].to_s != params[:user_password_confirm].to_s)
+			#return early if password doesn't match confirmation password
+			flash.message = "Your passwords did not match. Please try again."
+			erb :'users/new', :locals => {'body_class' => 'users users--new'}
+		
+		else
+			@new_user = User.new
+			@new_user.user_name = params[:user_name]
+			@new_user.user_email = params[:user_email].downcase
+			@new_user.is_admin = 0 #0 is false, 1 is true
+			@new_user.created = Time.now
+
+			password_salt = BCrypt::Engine.generate_salt
+			password_hash = BCrypt::Engine.hash_secret(params[:user_password], password_salt)
+
+			@new_user.password_salt = password_salt
+			@new_user.password_hash = password_hash
+
+			if @new_user.save
+				@users = User.order("lower(user_name) DESC").all
+				flash.message = "Success! You have created a new user."
+				erb :'users', :locals => {'body_class' => 'users users--new'}
+			else
+				flash.message = "Sorry, there was a problem creating the user. Please try again."
+				erb :'users/new', :locals => {'body_class' => 'users users--new'}
+			end
+		end
+
+	end
+
+	post '/delete' do
+		authorization_check
+
+		@nonce = Rack::Auth::Digest::Nonce.parse(params[:nonce])
+
+		@user = User.find(params[:user_id])
+		if @user.destroy
+			flash.message = "Success! You have destroyed a user."
+			@users = User.order("lower(user_name) DESC").all
+			erb :'users', :locals => {'body_class' => 'users'}
+		else
+			flash.message = "Sorry, there was a problem deleting the user. Please try again."
+			@users = User.order("lower(user_name) DESC").all
+			erb :'users', :locals => {'body_class' => 'users'}
+		end
+	end
 
 end
